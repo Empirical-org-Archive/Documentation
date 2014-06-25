487504bdf014474bb40835b288af08fcb3dce81d
Quinn Shanahan <q.shanahan@gmail.com>
2014-06-25 12:59:44 -0400

    publish wip

--- publish.sh
+++ publish.sh
@@ -2,4 +2,8 @@ git branch -D gh-pages
 git checkout --orphan gh-pages
 nanoc
 git rm --cached -r .
-git clean -df -e output -e .gitignore
+git clean -df -e output
+
+echo tmp         >> .gitignore
+echo crash.log   >> .gitignore
+echo .sass-cache >> .gitignore
