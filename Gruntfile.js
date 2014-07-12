1da555f504289b383c0dfcad311e9f3734f34f88
Quinn Shanahan <q.shanahan@gmail.com>
2014-07-12 09:36:10 -0400

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+  var conf = {
+    shell: {
+      nanoc: {
+        command: 'nanoc', options: { stdout: true }
+      }
+    },
+
+    watch: {
+      files: ["content/**", "layouts/**", "lib/**", "contents/**", "config.yaml", "nanoc.yaml", "Rules"],
+      tasks: ['shell']
+    },
+
+    concurrent: {
+      server: {
+        options: { logConcurrentOutput: true },
+        tasks: ['watch', 'connect']
+      }
+    },
+
+    connect: {
+      server: {
+        options: { base: 'output', port: 8080, keepalive: true }
+      }
+    }
+  };
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.initConfig(conf);
+  grunt.registerTask('default', ['concurrent']);
+};
